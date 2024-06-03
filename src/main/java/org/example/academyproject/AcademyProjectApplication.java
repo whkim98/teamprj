dfde28385cf451040f38ce05d4cdf2edbb88764b
hayooniiiiii <140838571+hayooniiiiii@users.noreply.github.com>
2024-06-03 15:12:12 +0900

    first commit

---
+++ src/main/java/org/example/academyproject/AcademyProjectApplication.java
@@ -0,0 +1,13 @@
+package org.example.academyproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AcademyProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AcademyProjectApplication.class, args);
+	}
+
+}
